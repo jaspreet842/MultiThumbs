66d310155444d54cb65f94984195d7e032bbe143
jaspreet842 <55507569+jaspreet842@users.noreply.github.com>
2020-08-24 01:45:36 +0530

    Created full website

--- js/services.js
+++ js/services.js
@@ -1,3 +1,9 @@
+// Go to top on refreshing the page.
+// $(document).ready(function () {
+//     $(this).scrollTop(0);
+// });
+
+// Moving bubbles in the background of the header section
 let width, height, largeContainer, canvas, ctx, circles, target, animateHeader = true;
 initHeader();
 addListeners();
@@ -75,12 +81,13 @@ function Circle() {
     };
 }
 
+// Slides of things which we do. It's timing funcion and, click and swipe events.
 let checkedSlide = 0;
-let buttons = document.getElementsByTagName('input');
-let slides = document.getElementsByTagName('label');
-let a = setInterval(()=>{
+let buttons = document.getElementsByClassName('radioBtns');
+let slides = document.getElementsByClassName('slideNo');
+let a = setInterval(() => {
     swipeLeft(checkedSlide)
-}, 3000);
+}, 4000);
 for (let i = 0; i < buttons.length; i++) {
     let startX = 0, endX = 0;
     slides[i].addEventListener('touchstart', (e) => {
@@ -102,24 +109,24 @@ for (let i = 0; i < buttons.length; i++) {
     slides[i].addEventListener("mouseup", mouseUp);
 }
 function swipeLeft(i) {
-    if(i!=buttons.length-1){
+    if (i != buttons.length - 1) {
         buttons[i + 1].checked = true;
         buttons[i].checked = false;
-        checkedSlide = i+1;
+        checkedSlide = i + 1;
     }
-    else{
+    else {
         buttons[0].checked = true;
         buttons[i].checked = false;
         checkedSlide = 0;
     }
 }
 function swipeRight(i) {
-    if(i!=0){
+    if (i != 0) {
         buttons[i - 1].checked = true;
         buttons[i].checked = false;
-        checkedSlide = i-1;
+        checkedSlide = i - 1;
     }
-    else{
+    else {
         buttons[5].checked = true;
         buttons[i].checked = false;
         checkedSlide = 5;
@@ -129,13 +136,57 @@ function mouseDown() {
     clearInterval(a);
 }
 function mouseUp() {
-    a = setInterval(()=>{
+    a = setInterval(() => {
         swipeLeft(checkedSlide)
-    }, 3000);
+    }, 4000);
 }
-document.addEventListener('keydown', (e)=>{
-    if(e.key=="ArrowRight")
+// Arrow key handling with slides.
+document.addEventListener('keydown', (e) => {
+    if (e.key == "ArrowLeft")
         swipeRight(checkedSlide);
-    else if(e.key == "ArrowLeft")
+    else if (e.key == "ArrowRight")
         swipeLeft(checkedSlide);
 });
+
+// Footer's Left menu bar
+function myFunction() {
+    var input, filter, ul, li, a, i;
+    input = document.getElementById("mySearch");
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("myMenu");
+    li = ul.getElementsByTagName("li");
+    for (i = 0; i < li.length; i++) {
+        a = li[i].getElementsByTagName("a")[0];
+        if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = "";
+        } else {
+            li[i].style.display = "none";
+        }
+    }
+}
+// Smiley in Footer's right section
+function smile() {
+    var a;
+    a = document.getElementById("div1");
+    a.innerHTML = "&#xf118;";
+    setTimeout(function () {
+        a.innerHTML = "&#xf11a;";
+    }, 1000);
+    setTimeout(function () {
+        a.innerHTML = "&#xf119;";
+    }, 2000);
+    setTimeout(function () {
+        a.innerHTML = "&#xf11a;";
+    }, 3000);
+}
+smile();
+setInterval(smile, 4000);
+var close = document.getElementsByClassName("closebtn");
+var i;
+for (i = 0; i < close.length; i++) {
+    close[i].onclick = function () {
+        var div = this.parentElement;
+        div.style.opacity = "0";
+        setTimeout(function () { div.style.display = "none"; }, 600);
+    }
+}
